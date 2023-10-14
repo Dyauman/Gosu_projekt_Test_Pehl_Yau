7d4959306ef79649e0fa464ed02177966694d460
CP154 <chris.pehl@web.de>
2023-10-14 13:11:09 +0200

    Strichmännchen Grundaufbau

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -58,29 +58,67 @@ public:
     GameWindow()
         : Window(1280, 720) { // Größe des Fensters
         set_caption("Nidhogg Fake");
-        }
-    
-    Player player1 = Player(300, 350); // Startposition des ersten Spielers
-    Player player2 = Player(700, 350); // Startposition des zweiten Spielers
+    }
+
+    Player player1 = Player(300, 300); // Startposition des ersten Spielers
+    Player player2 = Player(700, 300); // Startposition des zweiten Spielers
 
     void draw() override {
         // Anzeige der Spielerpositionen auf dem Bildschirm
         //Gosu::draw_text("Spieler 1: x = " + std::to_string(player1.x) + ", y = " + std::to_string(player1.y), 10, 10, 0xFFFFFFFF);
         //Gosu::draw_text("Spieler 2: x = " + std::to_string(player2.x) + ", y = " + std::to_string(player2.y), 10, 30, 0xFFFFFFFF);
 
-        Gosu::Font(24).draw_text("Geile Rampensau", 400, 300, 0,1,1);
-
+        Gosu::Font(24).draw_text("Nidhogg Fake", 400, 300, 0, 1, 1);
+        /*
         graphics().draw_line(
             player1.x, player1.y, Gosu::Color::RED,
             player1.x, player1.y - 100, Gosu::Color::RED,
             0.0
         );
+        
         graphics().draw_line(
             player2.x, player2.y, Gosu::Color::GREEN,
             player2.x, player2.y - 100, Gosu::Color::GREEN,
             0.0
         );
+        */
+        //Strichmännchen 1
+        graphics().draw_line(player1.x, player1.y, Gosu::Color::GREEN,
+            player1.x, player1.y + 100, Gosu::Color::GREEN,
+            0.0);  // Körper 400 300 400 400
+        graphics().draw_line(player1.x, player1.y + 50, Gosu::Color::GREEN,
+            player1.x + 30, player1.y - 20, Gosu::Color::GREEN,
+            0.0);  // rechter Arm 400 350 430 330
+        graphics().draw_line(player1.x, player1.y + 50, Gosu::Color::GREEN,
+            player1.x - 30, player1.y - 20, Gosu::Color::GREEN,
+            0.0);  // linker Arm 400 350 370 330
+        graphics().draw_line(player1.x, player1.y + 100, Gosu::Color::GREEN,
+            player1.x + 30, player1.y + 120, Gosu::Color::GREEN,
+            0.0);  // rechtes Bein 400 400 430 420
+        graphics().draw_line(player1.x, player1.y + 100, Gosu::Color::GREEN,
+            player1.x - 30, player1.y + 120, Gosu::Color::GREEN,
+            0.0);  // linkes Bein 400 400 370 420
+
+        //Strichmännchen 2
+        graphics().draw_line(player2.x, player2.y, Gosu::Color::WHITE,
+            player2.x, player2.y + 100, Gosu::Color::WHITE,
+            0.0);  // Körper 400 300 400 400
+        graphics().draw_line(player2.x, player2.y + 50, Gosu::Color::WHITE,
+            player2.x + 30, player2.y - 20, Gosu::Color::WHITE,
+            0.0);  // rechter Arm 400 350 430 330
+        graphics().draw_line(player2.x, player2.y + 50, Gosu::Color::WHITE,
+            player2.x - 30, player2.y - 20, Gosu::Color::WHITE,
+            0.0);  // linker Arm 400 350 370 330
+        graphics().draw_line(player2.x, player2.y + 100, Gosu::Color::WHITE,
+            player2.x + 30, player2.y + 120, Gosu::Color::WHITE,
+            0.0);  // rechtes Bein 400 400 430 420
+        graphics().draw_line(player2.x, player2.y + 100, Gosu::Color::WHITE,
+            player2.x - 30, player2.y + 120, Gosu::Color::WHITE,
+            0.0);  // linkes Bein 400 400 370 420
     }
+
+
+};
 /*
     void update() override {
         // Steuerung für Spieler 1
@@ -115,7 +153,7 @@ private:
     Player player2(100, 300);
 };
 */
-};
+
 
 int main() {
    GameWindow window;
