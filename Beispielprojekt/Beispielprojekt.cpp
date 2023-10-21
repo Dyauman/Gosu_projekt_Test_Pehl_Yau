ac80329116738e0141133466d26158ee1f85d1bb
Dicks <Dickson.Y@web.de>
2023-10-21 23:02:14 +0200

    Hitmarker, Blockieren der Player überkreuzung, Cooldown eingeführt, Koordinaten Klasse eingeführt, Player erbt Koordinate

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -3,9 +3,32 @@
 #include <cmath>
 #include<iostream>
 
+//Eigene Klasse fuer Koordinaten
+class Coordinates {
+public:
+    double x, y, hitmarker_x, hitmarker_y;
+    Coordinates(double startX, double startY, double hitX, double hitY) 
+        : x(startX), y(startY), hitmarker_x(hitX), hitmarker_y(hitY) 
+    {}
+
+    //Get Methoden um die Koordinaten von der Klasse zu bekommen
+    double getCoordinate_X()
+    {
+        return x;
+    }
+
+    double getCoordinate_hitX()
+    {
+        return hitmarker_x;
+    }
+};
+
+//Alter code teil fuer Player, kann entfernt werden wenn es nicht mehr benoetigt wird
+
+/*
 class Player {
 public:
-    double x, y;      
+    double x, y, marker1, marker2, marker3;      
     double direction; // 1 für rechts, -1 für links
     bool leftKeyPressed;
     bool rightKeyPressed;
@@ -17,21 +40,69 @@ public:
     {}
     void move(double direction) {
         this->x = this->x + direction;
+        this->marker1 = this->marker1 + direction;
+        this->marker2 = this->marker2 + direction;
+        this->marker3 = this->marker3 + direction;
     }
+};
+*/
+
+//Player Klasse mit Erbung der Koordinaten
+class Player : public Coordinates {
+public:
+    double direction; // 1 für rechts, -1 für links
+    bool leftKeyPressed;
+    bool rightKeyPressed;
+    int keyHold_r;      //Variable fuer kontinuierliche Bewegung
+    int keyHold_l;
+    
+
+    Player(double startX, double startY, double hitX, double hitY, bool left, bool right)
+        : Coordinates(startX,startY,hitX, hitY), leftKeyPressed(left), rightKeyPressed(right), keyHold_r(0), keyHold_l(0), direction(1)
+    {}
+
+    void move(double direction) {
+        this->x = this->x + direction;
+        this->hitmarker_x = this->hitmarker_x + direction;
+    }
+
+    //Methode fuer Angriff, falls Schwertposition groesser ist als gegnerischer Player => Hit
+    void hit(double swordPos, double hitmarkerPos)
+    {
+        
+        if (swordPos >= hitmarkerPos)
+        {
+            Gosu::Font(24).draw_text("You have been slained by xXPu$$ySl4y3rXx", 860, 400, 1, 1, 1);
+        }
+    }
+
 };
 
 class Sword {
 public:
     int swordPosition;
+    double swordLength_idle;
+    double swordLength_extended;
     bool upKeyPressed;
     bool downKeyPressed;
     bool extendingSword;
-    
-    Sword(int, bool, bool, bool)
-        : swordPosition(), upKeyPressed(), downKeyPressed(), extendingSword()
-    {}
-};
+    unsigned long cooldown;
 
+    Sword(int p, bool upKey, bool downKey, bool extending, double idle, double extended, unsigned long cd)
+        : swordPosition(p), upKeyPressed(upKey), downKeyPressed(downKey), extendingSword(extending), swordLength_idle(idle), swordLength_extended(extended), cooldown(cd)
+    {}
+
+    //Methode fuer die Schwertlaenge
+    double get_swordLength_extended()
+    {
+        return swordLength_extended;
+    }
+
+    double get_swordLength_idle()
+    {
+        return swordLength_idle;
+    }
+};
 
 class GameWindow : public Gosu::Window
 {
@@ -42,17 +113,17 @@ public:
          
     }
 
-    Player player1 = Player(300, 540, false, false); // Startposition des ersten Spielers
-    Player player2 = Player(1620, 540, false, false); // Startposition des zweiten Spielers
-    Sword swordPlayer1 = Sword(0, false, false, false);     //Schwertposition des ersten Spielers
-    Sword swordPlayer2 = Sword(0, false, false, false);    //Schwertposition des zweiten Spielers
+    Player player1 = Player(300, 540, 300, 540, false, false); // Startposition des ersten Spielers
+    Player player2 = Player(1620, 540, 1620, 540, false, false); // Startposition des zweiten Spielers
+    Sword swordPlayer1 = Sword(0, false, false, false, 70, 120, 0);     //Schwertposition des ersten Spielers
+    Sword swordPlayer2 = Sword(0, false, false, false, 70, 120, 0);    //Schwertposition des zweiten Spielers
 
     int steps = 50;                                 //Dynamische Variable fuer die Schrittweite
     int steps_divider = 5;                          //Dynamische Variable fuer mehrere Schritte
+  
 
     void draw() override {
-        Gosu::Font(24).draw_text("Nidhogg Fake", 860, 300, 0, 1, 1);
-
+        //Gosu::Font(24).draw_text("Nidhogg Fake", 860, 300, 1, 1, 1);
         //Player 1 Angriffsposition
         // Kopf
        if (swordPlayer1.extendingSword == true)
@@ -61,7 +132,7 @@ public:
 
             // Körper
             graphics().draw_line(player1.x, player1.y + 25, Gosu::Color::GREEN,
-                player1.x -20, player1.y + 100, Gosu::Color::GREEN,
+                player1.x - 20, player1.y + 100, Gosu::Color::GREEN,
                 0.0);
             // Schwertarm
             if (swordPlayer1.swordPosition == 2) {
@@ -71,11 +142,17 @@ public:
                     0.0);
                 //Schwert
                 graphics().draw_line(player1.x + 40, player1.y + 75, Gosu::Color::BLUE,
-                    player1.x + 80, player1.y + 75, Gosu::Color::BLUE,
+                    player1.x + swordPlayer1.swordLength_extended, player1.y + 75, Gosu::Color::BLUE,
                     0.0);
                 graphics().draw_line(player1.x + 45, player1.y + 85, Gosu::Color::BLUE,
                     player1.x + 45, player1.y + 65, Gosu::Color::BLUE,
                     0.0);
+            //Wenn die Schwertposition beim Angriff ungleich der Schwertposition des Gegners ist wird Methode aufgerufen
+                if (swordPlayer1.swordPosition != swordPlayer2.swordPosition)
+                {
+                    player1.hit((player1.getCoordinate_X() + swordPlayer1.get_swordLength_extended()), player2.getCoordinate_hitX());
+                }
+                
             }
             else if (swordPlayer1.swordPosition == 1) {
                 //Mitte
@@ -84,11 +161,16 @@ public:
                     0.0);
                 //Schwert
                 graphics().draw_line(player1.x + 40, player1.y + 50, Gosu::Color::BLUE,
-                    player1.x + 80, player1.y + 50, Gosu::Color::BLUE,
+                    player1.x + swordPlayer1.swordLength_extended, player1.y + 50, Gosu::Color::BLUE,
                     0.0);
                 graphics().draw_line(player1.x + 45, player1.y + 60, Gosu::Color::BLUE,
                     player1.x + 45, player1.y + 40, Gosu::Color::BLUE,
                     0.0);
+                //Wenn die Schwertposition beim Angriff ungleich der Schwertposition des Gegners ist wird Methode aufgerufen
+                if (swordPlayer1.swordPosition != swordPlayer2.swordPosition)
+                {
+                    player1.hit((player1.getCoordinate_X() + swordPlayer1.get_swordLength_extended()), player2.getCoordinate_hitX());
+                }
             }
             else if (swordPlayer1.swordPosition == 0) {
                 //Unten
@@ -97,11 +179,16 @@ public:
                     0.0);
                 //Schwert
                 graphics().draw_line(player1.x + 40, player1.y + 25, Gosu::Color::BLUE,
-                    player1.x + 80, player1.y + 25, Gosu::Color::BLUE,
+                    player1.x + swordPlayer1.swordLength_extended, player1.y + 25, Gosu::Color::BLUE,
                     0.0);
                 graphics().draw_line(player1.x + 45, player1.y + 35, Gosu::Color::BLUE,
                     player1.x + 45, player1.y + 15, Gosu::Color::BLUE,
                     0.0);
+                //Wenn die Schwertposition beim Angriff ungleich der Schwertposition des Gegners ist wird Methode aufgerufen
+                if (swordPlayer1.swordPosition != swordPlayer2.swordPosition)
+                {
+                    player1.hit((player1.getCoordinate_X() + swordPlayer1.get_swordLength_extended()), player2.getCoordinate_hitX());
+                }
             }
             // linker Arm 
             graphics().draw_line(player1.x -10 , player1.y + 50, Gosu::Color::GREEN,
@@ -124,6 +211,9 @@ public:
             graphics().draw_line(player1.x -20, player1.y + 100, Gosu::Color::GREEN,
                 player1.x - 50, player1.y + 120, Gosu::Color::GREEN,
                 0.0);
+
+            
+
         }
 
        //Player 1 Ruheposition
@@ -143,7 +233,7 @@ public:
                     0.0);
                 //Schwert
                 graphics().draw_line(player1.x + 15, player1.y + 75, Gosu::Color::BLUE,
-                    player1.x + 55, player1.y + 75, Gosu::Color::BLUE,
+                    player1.x + swordPlayer1.swordLength_idle, player1.y + 75, Gosu::Color::BLUE,
                     0.0);
                 graphics().draw_line(player1.x + 20, player1.y + 85, Gosu::Color::BLUE,
                     player1.x + 20, player1.y + 65, Gosu::Color::BLUE,
@@ -156,7 +246,7 @@ public:
                     0.0);
                 //Schwert
                 graphics().draw_line(player1.x + 15, player1.y + 50, Gosu::Color::BLUE,
-                    player1.x + 55, player1.y + 50, Gosu::Color::BLUE,
+                    player1.x + swordPlayer1.swordLength_idle, player1.y + 50, Gosu::Color::BLUE,
                     0.0);
                 graphics().draw_line(player1.x + 20, player1.y + 60, Gosu::Color::BLUE,
                     player1.x + 20, player1.y + 40, Gosu::Color::BLUE,
@@ -169,7 +259,7 @@ public:
                     0.0);
                 //Schwert
                 graphics().draw_line(player1.x + 15, player1.y + 25, Gosu::Color::BLUE,
-                    player1.x + 55, player1.y + 25, Gosu::Color::BLUE,
+                    player1.x + swordPlayer1.swordLength_idle, player1.y + 25, Gosu::Color::BLUE,
                     0.0);
                 graphics().draw_line(player1.x + 20, player1.y + 35, Gosu::Color::BLUE,
                     player1.x + 20, player1.y + 15, Gosu::Color::BLUE,
@@ -224,11 +314,16 @@ public:
                    0.0);
                //Schwert
                graphics().draw_line(player2.x - 40, player2.y + 75, Gosu::Color::BLUE,
-                   player2.x - 80, player2.y + 75, Gosu::Color::BLUE,
+                   player2.x - swordPlayer2.swordLength_extended, player2.y + 75, Gosu::Color::BLUE,
                    0.0);
                graphics().draw_line(player2.x - 45, player2.y + 85, Gosu::Color::BLUE,
                    player2.x - 45, player2.y + 65, Gosu::Color::BLUE,
                    0.0);
+               //Wenn die Schwertposition beim Angriff ungleich der Schwertposition des Gegners ist wird Methode aufgerufen
+               if (swordPlayer2.swordPosition != swordPlayer1.swordPosition)
+               {
+                   player2.hit((player2.getCoordinate_X() + swordPlayer2.get_swordLength_extended()), player1.getCoordinate_hitX());
+               }
            }
            else  if (swordPlayer2.swordPosition == 1) {
                //Mitte
@@ -237,11 +332,16 @@ public:
                    0.0);
                //Schwert
                graphics().draw_line(player2.x - 40, player2.y + 50, Gosu::Color::BLUE,
-                   player2.x - 80, player2.y + 50, Gosu::Color::BLUE,
+                   player2.x - swordPlayer2.swordLength_extended, player2.y + 50, Gosu::Color::BLUE,
                    0.0);
                graphics().draw_line(player2.x - 45, player2.y + 60, Gosu::Color::BLUE,
                    player2.x - 45, player2.y + 40, Gosu::Color::BLUE,
                    0.0);
+               //Wenn die Schwertposition beim Angriff ungleich der Schwertposition des Gegners ist wird Methode aufgerufen
+               if (swordPlayer2.swordPosition != swordPlayer1.swordPosition)
+               {
+                   player2.hit((player2.getCoordinate_X() + swordPlayer2.get_swordLength_extended()), player1.getCoordinate_hitX());
+               }
            }
            else if (swordPlayer2.swordPosition == 0) {
                //Oben
@@ -250,11 +350,16 @@ public:
                    0.0);
                //Schwert
                graphics().draw_line(player2.x - 40, player2.y + 25, Gosu::Color::BLUE,
-                   player2.x - 80, player2.y + 25, Gosu::Color::BLUE,
+                   player2.x - swordPlayer2.swordLength_extended, player2.y + 25, Gosu::Color::BLUE,
                    0.0);
                graphics().draw_line(player2.x - 45, player2.y + 35, Gosu::Color::BLUE,
                    player2.x - 45, player2.y + 15, Gosu::Color::BLUE,
                    0.0);
+               //Wenn die Schwertposition beim Angriff ungleich der Schwertposition des Gegners ist wird Methode aufgerufen
+               if (swordPlayer2.swordPosition != swordPlayer1.swordPosition)
+               {
+                   player2.hit((player2.getCoordinate_X() + swordPlayer2.get_swordLength_extended()), player1.getCoordinate_hitX());
+               }
            }
 
            // rechtes Bein
@@ -299,7 +404,7 @@ public:
                    0.0);
                //Schwert
                graphics().draw_line(player2.x - 15, player2.y + 75, Gosu::Color::BLUE,
-                   player2.x - 55, player2.y + 75, Gosu::Color::BLUE,
+                   player2.x - swordPlayer2.swordLength_idle, player2.y + 75, Gosu::Color::BLUE,
                    0.0);
                graphics().draw_line(player2.x - 20, player2.y + 85, Gosu::Color::BLUE,
                    player2.x - 20, player2.y + 65, Gosu::Color::BLUE,
@@ -312,7 +417,7 @@ public:
                    0.0);
                //Schwert
                graphics().draw_line(player2.x - 15, player2.y + 50, Gosu::Color::BLUE,
-                   player2.x - 55, player2.y + 50, Gosu::Color::BLUE,
+                   player2.x - swordPlayer2.swordLength_idle, player2.y + 50, Gosu::Color::BLUE,
                    0.0);
                graphics().draw_line(player2.x - 20, player2.y + 60, Gosu::Color::BLUE,
                    player2.x - 20, player2.y + 40, Gosu::Color::BLUE,
@@ -325,7 +430,7 @@ public:
                    0.0);
                //Schwert
                graphics().draw_line(player2.x - 15, player2.y + 25, Gosu::Color::BLUE,
-                   player2.x - 55, player2.y + 25, Gosu::Color::BLUE,
+                   player2.x - swordPlayer2.swordLength_idle, player2.y + 25, Gosu::Color::BLUE,
                    0.0);
                graphics().draw_line(player2.x - 20, player2.y + 35, Gosu::Color::BLUE,
                    player2.x - 20, player2.y + 15, Gosu::Color::BLUE,
@@ -381,7 +486,7 @@ public:
             player1.keyHold_l = 0;
         }
 
-        if (input().down(Gosu::KB_D)) {
+        if (input().down(Gosu::KB_D) && (player1.getCoordinate_X() < player2.getCoordinate_X() - 100)) {
             //Einzelne Bewegung
             if (!player1.rightKeyPressed) {
                 player1.rightKeyPressed = true;
@@ -403,7 +508,7 @@ public:
 
         // Steuerung für Spieler 2
 
-       if (input().down(Gosu::KB_LEFT)) {
+       if (input().down(Gosu::KB_LEFT) && (player2.getCoordinate_X() > player1.getCoordinate_X() + 100)) {
            //Einzelne Bewegung 
            if (!player2.leftKeyPressed) {
                 player2.leftKeyPressed = true;
@@ -490,7 +595,8 @@ public:
 
         // Player 1 Stich- & Ruheposition
         if (input().down(Gosu::KB_SPACE)) {
-            if (!swordPlayer1.extendingSword) {
+             if (!swordPlayer1.extendingSword && (Gosu::milliseconds() > swordPlayer1.cooldown + 1000)) {
+                swordPlayer1.cooldown = Gosu::milliseconds();
                 swordPlayer1.extendingSword = true;
             }
         }
@@ -500,7 +606,8 @@ public:
 
         // Player 2 Stich- & Ruheposition
         if (input().down(Gosu::KB_RETURN)) {
-            if (!swordPlayer2.extendingSword) {
+            if (!swordPlayer2.extendingSword && (Gosu::milliseconds() > swordPlayer2.cooldown + 1000)) {
+                swordPlayer2.cooldown = Gosu::milliseconds();
                 swordPlayer2.extendingSword = true;
             }
         }
@@ -508,6 +615,9 @@ public:
             swordPlayer2.extendingSword = false;
         }
     }
+
+
+    
 };
 
 
